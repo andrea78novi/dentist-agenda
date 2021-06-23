a0a33e75ee8175d714a2fdbf36180d5e3dd9f9cd
andrea78novi <andrea78novi@gmail.com>
2021-06-23 17:55:55 +0200

    Removed MongoDB URL

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 const config = {
    port: 3000,
-   dbConnection: 'mongodb+srv://andrea78novi:4zDxpw9aFVuMlJfG@cluster0.rmmy6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+   dbConnection: '<PUT_YOUR_MONGODB_URL_HERE>'
 };
 
 export default config;
